6cf9cae76ec98bc5e70e2db8fae477efd5490bcb
Front-projects <sasnykvlad2@gmail.com>
2024-07-23 18:44:21 +0300

    upd

--- src/components/Lottery/LockedLottery.jsx
+++ src/components/Lottery/LockedLottery.jsx
@@ -5,10 +5,10 @@ export default function LockedLottery({ lottery }) {
   return (
     <>
       <div className="w-full h-full top-0 absolute z-1 h-full ">
-        <div className="h-1/2 rounded-[23px] border-2 border-white border-b-[0px] bg-[#0F1511]/90 flex items-center justify-center flex-col pb-8 text-center text-[20px] px-8 max-h-660:px-2 max-h-660:text-[12px]">
+        <div className="h-1/2 rounded-[23px] border-2 border-white border-b-[0px] bg-[#0F1511]/95 flex items-center justify-center flex-col pb-8 text-center text-[20px] px-8 max-h-660:px-2 max-h-660:text-[12px]">
           TAKE PART IN THE LOTTERY AND MAKE A PROFIT
         </div>
-        <div className="h-1/2 rounded-[23px] border-2 border-white border-t-[0px] bg-[#0F1511]/90 pt-2 w-full  flex items-center justify-center px-6">
+        <div className="h-1/2 rounded-[23px] border-2 border-white border-t-[0px] bg-[#0F1511]/95 pt-2 w-full  flex items-center justify-center px-6">
           <Button>BUY {lottery.price} $</Button>
         </div>
       </div>
