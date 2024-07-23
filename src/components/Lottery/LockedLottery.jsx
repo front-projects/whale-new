9899d2ea8c15eccdac7fd0fc66db6e0e1c013d11
Front-projects <sasnykvlad2@gmail.com>
2024-07-23 18:42:55 +0300

    upd

--- src/components/Lottery/LockedLottery.jsx
+++ src/components/Lottery/LockedLottery.jsx
@@ -4,11 +4,11 @@ import { LockIcon } from "../UI/icons";
 export default function LockedLottery({ lottery }) {
   return (
     <>
-      <div className="w-full h-full top-0 absolute z-1 h-full locked-lottery">
-        <div className="h-1/2 rounded-[23px] border-2 border-white border-b-[0px] bg-[#0F15119E] flex items-center justify-center flex-col pb-8 text-center text-[20px] px-8 max-h-660:px-2 max-h-660:text-[12px]">
+      <div className="w-full h-full top-0 absolute z-1 h-full ">
+        <div className="h-1/2 rounded-[23px] border-2 border-white border-b-[0px] bg-[#0F1511]/90 flex items-center justify-center flex-col pb-8 text-center text-[20px] px-8 max-h-660:px-2 max-h-660:text-[12px]">
           TAKE PART IN THE LOTTERY AND MAKE A PROFIT
         </div>
-        <div className="h-1/2 rounded-[23px] border-2 border-white border-t-[0px] bg-[#0F15119E] pt-2 w-full  flex items-center justify-center px-6">
+        <div className="h-1/2 rounded-[23px] border-2 border-white border-t-[0px] bg-[#0F1511]/90 pt-2 w-full  flex items-center justify-center px-6">
           <Button>BUY {lottery.price} $</Button>
         </div>
       </div>
