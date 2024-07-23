e6d01d4325467a578eddce5818d80cfc248e2be1
Front-projects <sasnykvlad2@gmail.com>
2024-07-23 18:32:41 +0300

    upd

--- src/components/Lottery/LockedLottery.jsx
+++ src/components/Lottery/LockedLottery.jsx
@@ -4,12 +4,12 @@ import { LockIcon } from "../UI/icons";
 export default function LockedLottery({ lottery }) {
   return (
     <>
-      <div className="w-full h-full absolute top-0">
+      <div className="w-full h-full absolute top-0 z-10">
         <div className="z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-[50%] lock-icon w-[100px] h-[100px] max-h-660:w-[70px] max-h-660:h-[70px] flex items-center justify-center">
           <LockIcon />
         </div>
       </div>
-      <div className="w-full h-full top-0 absolute z-3 h-full">
+      <div className="w-full h-full top-0 absolute z-1 h-full">
         <div className="z-0 h-1/2 rounded-[23px] border-2 border-white border-b-[0px] bg-[#0F15119E] flex items-center justify-center flex-col pb-8 text-center text-[20px] px-8 locked-lottery max-h-660:px-2 max-h-660:text-[12px]">
           TAKE PART IN THE LOTTERY AND MAKE A PROFIT
         </div>
