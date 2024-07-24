f16a6db2d494a4a12e463f4bbfae244a9c0abb8e
Front-projects <sasnykvlad2@gmail.com>
2024-07-24 12:31:19 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
       WebApp.ready();
       console.log("Running inside Telegram Web App");
     } else {
-      WebApp.Link("https://telegram.org/");
+      WebApp.openLink("https://telegram.org/");
     }
   }, []);
 
