fd2ca8bc3de1ea0b9b16b3f1c78cfcc68dabd0b7
Front-projects <sasnykvlad2@gmail.com>
2024-07-24 19:11:38 +0300

    upd

--- src/components/Lottery/HowToEarn.jsx
+++ src/components/Lottery/HowToEarn.jsx
@@ -28,14 +28,16 @@ export default function HowToEarn() {
         HOW TO EARN <TouchIcon />
       </Button>
       <Modal faq isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <div className="text-[24px]">How to earn ?</div>
+        <div className="text-[28px]">How to earn ?</div>
         <div className="text-[14px] mt-4 flex flex-col gap-2 text-start px-1">
           <p>
             Welcome. We kindly ask you to read this text from beginning to end
             in order to understand the rules of the game and get maximum
             results. Thank you.{" "}
           </p>
-          <h2>How the lottery works and how to get started: </h2>
+          <h2 className="text-[20px] text-center">
+            How the lottery works and how to get started:{" "}
+          </h2>
           <p>
             Every day we give away large amounts of money to our players. By
             buying a ticket, you get a chance to win big money and receive
@@ -57,7 +59,9 @@ export default function HowToEarn() {
             distribution script. The system randomly selects a different number
             of participants and sends random amounts automatically.{" "}
           </p>
-          <p>Example of winnings for different tickets:</p>
+          <p className="text-[20px] text-center">
+            Example of winnings for different tickets:
+          </p>
           <p>
             By buying a ticket for $15, you have a chance to win from $0.50 to
             $1000+ every day.{" "}
@@ -74,7 +78,7 @@ export default function HowToEarn() {
             By buying all the tickets at the same time, you participate in
             several lotteries at once. Logically, you can win more often.{" "}
           </p>
-          <p>Basic concepts: </p>
+          <p className="text-[20px] text-center">Basic concepts: </p>
           <p>- Each ticket is an independent lottery</p>
           <p>- The ticket is purchased once and remains active forever</p>
           <p>
@@ -96,7 +100,9 @@ export default function HowToEarn() {
             - After the purchase, you do not need to do anything to receive the
             winnings. Join our chat, follow the group and check out the game.
           </p>
-          <p>How to increase the chance of winning and earn now: </p>
+          <p className="text-[20px] text-center">
+            How to increase the chance of winning and earn now:{" "}
+          </p>
           <p>
             Each invited person using your referral link will increase your
             chance of winning by 2% in the general system.
