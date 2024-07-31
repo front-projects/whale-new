7773f569c4fb40a3ce9d8c561de0790bf4a5013d
Front-projects <sasnykvlad2@gmail.com>
2024-07-31 10:15:57 +0300

    upd

--- src/components/Lottery/HowToEarn.jsx
+++ src/components/Lottery/HowToEarn.jsx
@@ -28,14 +28,16 @@ export default function HowToEarn() {
         HOW TO EARN <TouchIcon />
       </Button>
       <Modal faq isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <div className="text-[28px]">How to earn ?</div>
+        <div className="text-[28px] history-plus-gradient py-2">
+          How to earn ?
+        </div>
         <div className="text-[14px] mt-4 flex flex-col gap-2 text-start px-1">
           <p>
             Welcome. We kindly ask you to read this text from beginning to end
             in order to understand the rules of the game and get maximum
             results. Thank you.{" "}
           </p>
-          <h2 className="text-[20px] text-center">
+          <h2 className="text-[20px] text-center history-plus-gradient py-3">
             How the lottery works and how to get started:{" "}
           </h2>
           <p>
@@ -59,7 +61,7 @@ export default function HowToEarn() {
             distribution script. The system randomly selects a different number
             of participants and sends random amounts automatically.{" "}
           </p>
-          <p className="text-[20px] text-center">
+          <p className="text-[20px] text-center history-plus-gradient py-3">
             Example of winnings for different tickets:
           </p>
           <p>
@@ -78,7 +80,9 @@ export default function HowToEarn() {
             By buying all the tickets at the same time, you participate in
             several lotteries at once. Logically, you can win more often.{" "}
           </p>
-          <p className="text-[20px] text-center">Basic concepts: </p>
+          <p className="text-[20px] text-center history-plus-gradient py-3">
+            Basic concepts:{" "}
+          </p>
           <p>- Each ticket is an independent lottery</p>
           <p>- The ticket is purchased once and remains active forever</p>
           <p>
@@ -100,7 +104,7 @@ export default function HowToEarn() {
             - After the purchase, you do not need to do anything to receive the
             winnings. Join our chat, follow the group and check out the game.
           </p>
-          <p className="text-[20px] text-center">
+          <p className="text-[20px] text-center history-plus-gradient py-3">
             How to increase the chance of winning and earn now:{" "}
           </p>
           <p>
